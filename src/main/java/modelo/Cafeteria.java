daaacd860954dec8084b606af5ea7f4556de9336
JsFrancisc0 <j.saldias03@ufromail.cl>
2023-11-29 15:10:12 -0300

    jvhvvhj

--- src/main/java/modelo/Cafeteria.java
+++ src/main/java/modelo/Cafeteria.java
@@ -50,4 +50,8 @@ public class Cafeteria {
     public void agregarCafe(Cafe cafe){
         getListaCafes().add(cafe);
     }
+
+    public void eliminarCafe(Cafe cafe){
+        getListaCafes().remove(cafe);
+    }
 }
